d49d00ca870afe4df5fc72795787c01b1b56bceb
Lucas Courot <lucas@courot.com>
2014-01-06 16:32:44 +0100

    Add clients counter

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var port     = 1337;
 var title    = 'Webchat';
 var messages = [{'username': 'Lucas Courot', 'message': 'Hello world!'}];
 
-var http = require('http');
-var url  = require('url');
-var fs   = require('fs');
-var io   = require('socket.io');
+var http      = require('http');
+var url       = require('url');
+var fs        = require('fs');
+var io        = require('socket.io');
+var nbClients = 0;
 
 var server = http.createServer(function (request, response) {
     var path = url.parse(request.url).pathname;
@@ -31,10 +32,15 @@ io = io.listen(server);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('fetchMessages', messages);
+    io.sockets.emit('fetchNbClients', ++nbClients);
 
     socket.on('newMessage', function (newMessage) {
         messages.push(newMessage);
         socket.broadcast.emit('fetchNewMessage', newMessage);
+    });
+
+    socket.on("disconnect", function(){
+        io.sockets.emit('fetchNbClients', --nbClients);
     });
 });
 
