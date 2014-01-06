b7055fc97f3d045f99a5a2c1b412fba20c71a3c5
Lucas Courot <lucas@courot.com>
2014-01-06 15:59:03 +0100

    Add socket.io

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var messages = [{'username': 'Lucas Courot', 'message': 'Hello world!'}];
 var http = require('http');
 var url  = require('url');
 var fs   = require('fs');
-var SSE  = require('sse');
-var EventEmitter  = require('events').EventEmitter;
-var eventEmitter = new EventEmitter();
+var io   = require('socket.io');
 
 var server = http.createServer(function (request, response) {
     var path = url.parse(request.url).pathname;
 
-    // The homepage
+    // Homepage
     if (path == '/') {
         fs.readFile('./index.html', function (error, content) {
             if (error) {
@@ -23,45 +21,21 @@ var server = http.createServer(function (request, response) {
                 response.end(content, 'utf-8');
             }
         });
-    } else if (path == '/messages') { // The chat API
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-
-        if (request.method.toLowerCase() == 'post') { // Add new message
-            var requestBody = '';
-
-            request.on('data', function (data) {
-                requestBody += data;
-            });
-
-            request.on('end', function () {
-                var newMessage = eval('(' + requestBody + ')');
-                messages.unshift(newMessage);
-
-                eventEmitter.emit('sendToClients', newMessage);
-            });
-
-            response.write("{'errors':'Message added'}");
-        } else if (request.method.toLowerCase() == 'get') { // Retrieve all the messages
-            response.write(JSON.stringify(messages));
-        } else {
-            response.writeHead(405);
-            response.end('Method not allowed');
-        }
-
-        response.end();
-    } else if (path == '/sse') { // SSE
-        var sse = new SSE(server);
-        sse.on('connection', function(client) {
-            eventEmitter.on('sendToClients', function (message) {
-                console.log(message);
-                client.send(JSON.stringify(messages));
-            });
-        });
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end('Not found.');
     }
-    else {
-            response.writeHead(404, { 'Content-Type': 'text/html' });
-            response.end('Not found.');
-        }
 }).listen(port);
+
+io = io.listen(server);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('fetchMessages', messages);
+
+    socket.on('newMessage', function (newMessage) {
+        messages.push(newMessage);
+        socket.broadcast.emit('fetchNewMessage', newMessage);
+    });
+});
 
 console.log('Server running on port ' + port);
